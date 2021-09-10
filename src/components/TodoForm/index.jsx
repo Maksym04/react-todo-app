6c2c6495387cc4cc1b92f179273d651c4e20d517
Maksim <max.alekseev04@gmail.com>
2021-09-10 16:09:28 +0300

    Fix TodoForm

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -54,7 +54,6 @@ function TodoForm () {
                       <input
                         {...field}
                         placeholder='Enter todo here'
-                        autoFocus
                         className={inputClassNames}
                       />
                     );
