2f38fe01aa414416ca8cda9bc5d2118c748c1449
Maksim <max.alekseev04@gmail.com>
2021-09-10 13:39:50 +0300

    Fix Add.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,8 @@ import './App.css';
 import React from 'react';
 import TodoPage from './pages/TodoPage';
 
-const themes = {
-  light: {
-    background: '#1E90FF',
-  },
-  dark: {
-    background: '#222222',
-  },
-};
-
-export const ThemeContext = React.createContext(themes.light);
-
 function App () {
-  return (
-    <ThemeContext.Provider value={themes.dark}>
-      <TodoPage />
-    </ThemeContext.Provider>
-  );
+  return <TodoPage />;
 }
 
 export default App;
